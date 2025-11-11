06968074db05153899e3ff0ffc7110e13189fe13
Yip <david.barghoudian@gmail.com>
2025-11-11 15:54:17 +0100

    Update ui.core.js

--- ui.core.js
+++ ui.core.js
@@ -1475,7 +1475,7 @@ if (newPathBtn) {
 
   // --- Init on load ---
   (async () => {
-    const REMOTE_JSON_URL = 'https://yakmandji.github.io/Grim-dawn-map-marker-tool/gdmm_all_profiles.json?v=2.2';
+    const REMOTE_JSON_URL = 'https://yakmandji.github.io/Grim-dawn-map-marker-tool/gdmm_all_profiles.json?v=2.3';
     // empty base
     state.profiles['Profil 1'] = { markers:[], map:{}, created: new Date().toISOString(), updated: new Date().toISOString() };
     setActiveProfile('Profil 1');
