c7a73c5956dd18da07f20f93cffc96cc5bf5cdb4
Yip <david.barghoudian@gmail.com>
2025-11-14 12:28:07 +0100

    Add files via upload

---
+++ ui.navmarkers.js
@@ -0,0 +1,30 @@
+// ui.navmarkers.js
+(function () {
+  window.NAV_MARKERS_BY_SIZE = {
+    // map Cairn (8948x9133)
+    '8948x9133': [
+      // 1) Téléport
+      {
+        xp: 37.5,
+        yp: 63.94,
+        icon: 'img/icon-eye.png',
+        targetProfile: 'Korvan Basin',
+        targetXp: 20.11,
+        targetYp: 95.22,
+        targetScale: 1.2
+      },
+
+      // 2) Link other profile
+/*      {
+        xp: 80.0,
+        yp: 10.0,
+        icon: 'img/icon-eye.png',
+        targetProfile: 'Malmouth',
+        targetXp: 65.0,
+        targetYp: 40.0,
+        targetScale: 1.2
+      }*/
+    ],
+
+  };
+})();
