a08e4b33fe2485dde516495b3914928656c330bd
Yip <david.barghoudian@gmail.com>
2025-11-14 13:05:52 +0100

    Update ui.navmarkers.js

--- ui.navmarkers.js
+++ ui.navmarkers.js
@@ -3,7 +3,7 @@
   window.NAV_MARKERS_BY_SIZE = {
     // map Cairn (8948x9133)
     '8948x9133': [
-      // 1) Téléport
+      // Home stade rift
       {
         xp: 37.5,
         yp: 63.94,
@@ -13,17 +13,50 @@
         targetYp: 95.22,
         targetScale: 1.2
       },
-
-      // 2) Link other profile
-/*      {
-        xp: 80.0,
-        yp: 10.0,
-        icon: 'img/icon-eye.png',
-        targetProfile: 'Malmouth',
-        targetXp: 65.0,
-        targetYp: 40.0,
+      // Téléport Devil Crossing
+      {
+        xp: 62.89,
+        yp: 89.74,
+        icon: 'img/icon-link.png',
+        targetProfile: 'Korvan Basin',
+        targetXp: 19.94,
+        targetYp: 98.47,
         targetScale: 1.2
-      }*/
+      },
+      // Téléport Conflagration
+      {
+        xp: 39.26,
+        yp: 60.25,
+        icon: 'img/icon-link.png',
+        targetXp: 41.32,
+        targetYp: 28.12,
+        targetScale: 1.2
+      },
+      // Téléport Fort Ikon
+      {
+        xp: 15.13,
+        yp: 22.44,
+        icon: 'img/icon-link.png',
+        targetProfile: 'Korvan Basin',
+        targetXp: 19.94,
+        targetYp: 98.47,
+        targetScale: 1.2
+      },            
+    ],
+
+
+    // map Korvan Basin (5427x5553)
+    '5427x5553': [
+      // 1) Téléport
+      {
+        xp: 19.94,
+        yp: 98.47,
+        icon: 'img/icon-link.png',
+        targetProfile: 'Cairn',
+        targetXp: 62.90,
+        targetYp: 89.81,
+        targetScale: 1.2
+      },
     ],
 
   };
