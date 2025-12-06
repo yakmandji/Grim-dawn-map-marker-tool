bca9c4c38e219fae48831053f26345f53b1d3d02
yakmandji <david.barghoudian@gmail.com>
2025-12-06 18:24:13 +0100

    rename region

--- js/ui.overlay.js
+++ js/ui.overlay.js
@@ -4,7 +4,7 @@ window.DUNGEON_OVERLAYS = [
 { id: "corrupted_tomb", map: "cairn", img: "corrupted-tomb.jpg", left: 37.8938, top: 87.1456, width: 5.3531, height: 5.9455 },
 { id: "suffering_angish", map: "cairn", img: "suffering-angish.jpg", left: 43.7098, top: 90.0681, width: 11.4045, height: 11.1742 },
 { id: "staunton_mine", map: "cairn", img: "staunton-mine.jpg", left: 55.3475, top: 91.9166, width: 4.4269, height: 4.1056 },
-{ id: "forgotten_depths", map: "cairn", img: "forgotten-depths.jpg", left: 32.9228, top: 80.78493, width: 5.2, height: 5.1 },
+{ id: "forgotten_depths", map: "cairn", img: "the-forgotten-depths.jpg", left: 32.9228, top: 80.78493, width: 5.2, height: 5.1 },
 
 { id: "bloodbriars_lair", map: "cairn", img: "bloodbriars-lair.jpg", left: 34.2228, top: 76.08493, width: 3.5, height: 4 },
 
