cbae9a8eec8ae4e2607b24b0d1ca8d06ec26bf20
yakmandji <david.barghoudian@gmail.com>
2025-12-20 00:45:36 +0100

    add more zoom

--- js/ui.core.js
+++ js/ui.core.js
@@ -449,7 +449,7 @@ function renderMarkers(options = {}) {
     const vb = viewport.getBoundingClientRect();
     const pt = pctToPx(xp, yp);
 
-    const scale = clamp(targetScale || state.view.scale, 0.35, 1.30);
+    const scale = clamp(targetScale || state.view.scale, 0.35, 1.50);
     state.view.scale = scale;
 
     state.view.x = vb.width  / 2 - pt.x * scale;
@@ -772,7 +772,7 @@ viewport.addEventListener('pointermove', e => {
           const targetScale = clamp(
             pinchState.startScale * ratio,
             0.35,
-            1.30
+            1.50
           );
           const vb = viewport.getBoundingClientRect();
           const centerNow = midpoint(p1, p2);
@@ -863,7 +863,7 @@ viewport.addEventListener('pointermove', e => {
   //ZOOM FONCTION
     function zoomAt(clientX, clientY, step) {
       const old = state.view.scale;
-      const ns = clamp(old * (1 + step), 0.35, 1.30);
+      const ns = clamp(old * (1 + step), 0.35, 1.50);
       if (ns === old) return;
       const vb = viewport.getBoundingClientRect();
       const ox = clientX - vb.left;
