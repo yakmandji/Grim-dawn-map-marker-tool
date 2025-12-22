d405fb3afceea9798333638c9e5b6b58c8022c50
yakmandji <david.barghoudian@gmail.com>
2025-12-22 02:16:54 +0100

    update region

--- js/ui.map.base.js
+++ js/ui.map.base.js
@@ -39,7 +39,7 @@
     { id:'Bastion-order', w: 68, h: 75, img: 'img/qol/bastion-order.png', xp: 31.95, yp: 58.64,  anchor: 'center' },
     { id:'kymon-sanctuary', w: 70, h: 75, img: 'img/qol/kymon-sanctuary.png', xp: 34.34, yp: 57.40,  anchor: 'center' },
     { id:'stonerend-quarry', w: 64, h: 70, img: 'img/qol/stonerend-quarry.png', xp: 32.8, yp: 49.55,  anchor: 'center' },
-    { id:'fort-ikon', w: 75, h: 80, img: 'img/qol/fort-ikon.png', xp: 15, yp: 21.55,  anchor: 'center' },
+    { id:'fort-ikon', w: 75, h: 80, img: 'img/qol/fort-ikon.png?1.0', xp: 15.10, yp: 21.51,  anchor: 'center' },
 
 
   ];
