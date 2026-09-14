e05a198ae0c02a5626a9d9b75d9f666c33f65d1b
yakmandji <david.barghoudian@gmail.com>
2026-09-14 20:13:10 +0200

    add region Asterkarn

--- js/ui.core.js
+++ js/ui.core.js
@@ -1317,7 +1317,7 @@ viewport.addEventListener('pointermove', e => {
     'Cairn':        'https://www.grimcustommarker.org/maps/cairn_profile.json?v=1.17',
     'Malmouth':     'https://www.grimcustommarker.org/maps/malmouth_profile.json?v=1.13',
     'Korvan Basin': 'https://www.grimcustommarker.org/maps/korvan_basin_profile.json?v=1.15',
-    'Asterkarn':    'https://www.grimcustommarker.org/maps/asterkarn_profile.json?v=1.10',
+    'Asterkarn':    'https://www.grimcustommarker.org/maps/asterkarn_profile.json?v=1.11',
   };
 
   // Load map for profile if need
